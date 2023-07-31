f522a26a9658d000c7364a892b5ee9d417ddfcf0
SRchris <100946291+SRchris@users.noreply.github.com>
2023-07-31 15:59:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ def main():
 
     # Create a selection box for page navigation
     pages = {
-        'Slippage Page': show_slippage_page
+        'Slippage Page': 'Slippage Page',
+        'Page 2': 'Page 2',  # Placeholder for additional pages
+        'Page 3': 'Page 3',  # Placeholder for additional pages
         # Add more pages here if you have additional pages
     }
     page_selection = st.sidebar.selectbox('Go to', list(pages.keys()))
 
     # Display the selected page
-    if page_selection in pages:
-        pages[page_selection]()
+    if page_selection == 'Slippage Page':
+        show_slippage_page()
+    elif page_selection == 'Page 2':
+        st.write("This is Page 2. Add your content here.")
+    elif page_selection == 'Page 3':
+        st.write("This is Page 3. Add your content here.")
+    # Add more conditions for additional pages here
 
 if __name__ == "__main__":
     main()
